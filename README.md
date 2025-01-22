cac782c043bf65592635e6d748398847d2e9c4ba
wramosquispe <93456532+wramosquispe@users.noreply.github.com>
2025-01-21 22:36:12 -0500

    Portafolio ALURA
    Este proyecto esta en construcción, se a tomado como punto de partida un proyecto de una colega participante

---
+++ README.md
@@ -0,0 +1,33 @@
+# 📁 My Portafolio
+Welcome to my Personal portafolio! 🖐 Here you can explore my experience as a developer. Check out the projects I've worked on and know more about me.
+
+ ![Captura de pantalla 2024-10-14 - 11 08 36](https://github.com/user-attachments/assets/e45752bf-7238-4891-b568-7fabd5411f1e)
+
+## 🔧 Key Features
+### 🌑 Dark mode
+The portfolio automatically adapts to your browser's theme settings, switching between light and dark modes for an optimal viewing experience based on your preferences.
+
+![Captura de pantalla 2024-10-14 - 11 08 25](https://github.com/user-attachments/assets/2acb7b15-e762-47e0-8746-e999cef23962)
+
+### 🌐 English/Spanish translation
+You can easily switch between English and Spanish.
+
+
+![Captura de pantalla 2024-10-14 - 11 10 11](https://github.com/user-attachments/assets/c113b079-c828-4d79-8af9-3ca90ddc2cf1)
+
+### 📱 Fully Responsive Design
+
+The site is optimized for all devices, from desktops to mobile phones. 
+
+<img src="https://github.com/user-attachments/assets/6c7b3b2e-1f9e-4ff1-81a9-c4d3dc4e32a8" width="200" alt="Mobile view"> <img src="https://github.com/user-attachments/assets/981897bc-315f-4a82-96ad-cad1c13323cf" width="267" alt="Tablet view">
+
+## 💻Used Technologies
+* HTML for structured content
+* CSS for design and layout
+* JavaScript for dynamic interactivity
+* i18next for language translation
+
+### 🚀 Live Demo
+
+Check out the live version of my portfolio <a href="https://abigailsalazar.github.io/Portafolio/" target="_blank">here</a>.
+
---
+++ data.js
@@ -0,0 +1,169 @@
+const projects = [
+    {
+        "id": 9,
+        "title": "Space App, Space Galery - 2024",
+        "code-link": "https://github.com/AbigailSalazar/Space-App",
+        "demo-link": "https://space-app-teal-five.vercel.app/",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/Alura-Space.png"
+          }
+        ],
+        "technologies": [
+          "react",
+          "javascript",
+          "css"
+        ],
+        "translation-key": "projects.spaceApp"
+      },
+      {
+        "id": 8,
+        "title": "Alura Org, Team Management - 2024",
+        "code-link": "https://github.com/AbigailSalazar/Alura-org",
+        "demo-link": "https://alura-org-taupe.vercel.app/",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/Alura-Org.png"
+          }
+        ],
+        "technologies": [
+          "react",
+          "javascript",
+          "css"
+        ],
+        "translation-key": "projects.aluraOrg"
+      },
+      {
+        "id": 7,
+        "title": "Alura Geek, Product Management - 2024",
+        "code-link": "https://github.com/AbigailSalazar/AluraGeek",
+        "demo-link": "https://alura-geek-sandy.vercel.app/",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/Alura-Geek.png"
+          }
+        ],
+        "technologies": [
+          "javascript",
+          "css"
+        ],
+        "translation-key": "projects.aluraGeek"
+      },
+      {
+        "id": 6,
+        "title": "Text Encrypter - 2024",
+        "code-link": "https://github.com/AbigailSalazar/Encriptador-de-texto",
+        "demo-link":"https://abigailsalazar.github.io/Encriptador-de-texto/",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/text-encrypter.png"
+          }
+        ],
+        "technologies": [
+          "javascript",
+          "html5",
+          "css"
+        ],
+        "translation-key": "projects.encrypter"
+      },
+      {
+        "id": 5,
+        "title": "\"FavQuotes\" save your favorite quotes - 2024",
+        "code-link": "https://github.com/AbigailSalazar/FavQuotesFront",
+        "demo-link":"https://fav-quotes.netlify.app/",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/fav-quotes.png"
+          }
+        ],
+        "technologies": [
+          "angular",
+          "bootstrap",
+          "javascript",
+          "node-js",
+          "postman",
+          "mongodb"
+        ],
+        "translation-key": "projects.favquotes"
+      },
+      {
+        "id": 4,
+        "title": "\"Venvi\" Second hand shop - 2024",
+        "code-link": "https://github.com/AbigailSalazar/Venvi-front/tree/main",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/venvi1.png"
+          }
+        ],
+        "technologies": [
+          "node-js",
+          "javascript",
+          "html5",
+          "css",
+          "postman",
+          "firebase",
+          "mongodb"
+        ],
+        "translation-key": "projects.venvi"
+      },
+      {
+        "id": 3,
+        "title": "\"SquadTrek \" Group travel mobile app - 2023",
+        "code-link": "https://github.com/AbigailSalazar/SquadTrek",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/SquadTrek.png"
+          }
+        ],
+        "technologies": [
+          "android-studio",
+          "firebase"
+        ],
+        "translation-key": "projects.squadtrek"
+      },
+      {
+        "id": 2,
+        "title": "\"lovit\" Product management - 2023",
+        "code-link": "https://github.com/israelparra/lovit-proyecto",
+        "img": [
+          {
+            "id": 0,
+            "path": "assets/img-projects/lovit2.png"
+          }
+        ],
+        "technologies": [
+          "spring",
+          "mysql"
+        ],
+        "translation-key": "projects.lovit"
+      }
+      
+]
+
+const technologies = {
+  javascript: "js.png",
+  css: "css-3.png",
+  java: "java.png",
+  java_fx: "java-fx.png",
+  spring: "spring.png",
+  react: "react.svg",
+  angular: "angular.svg",
+  "node-js": "nodo-js.png",
+  html: "html-5.png",
+  bootstrap: "bootstrap.svg",
+  postman: "postman-icon.svg",
+  firebase: "firebase.svg",
+  "android-studio": "android_studio.png",
+  mysql: "mysql.png",
+  mongodb: "mongodb-icon-2.svg"
+};
+
+
+export  {projects,technologies}
